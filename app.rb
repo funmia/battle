aad9fa7c77496cb3da821ccaac27737c395ea2f5
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-18 15:40:10 +0100

    Add test infrastructure setup

--- app.rb
+++ app.rb
@@ -1,10 +1,9 @@
 require 'sinatra/base'
-# require 'shotgun'
 
 class Battle < Sinatra::Base
   get '/' do
-    "Hello Battle!"
+    "Testing infrastructure working!"
   end
 
   run! if app_file == $0
-end 
+end
