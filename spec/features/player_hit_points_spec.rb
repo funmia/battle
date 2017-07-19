c402f8016723e671ef12ce1aaaaf1e14980d81cd
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-07-19 12:54:49 +0100

    test helpers

--- spec/features/player_hit_points_spec.rb
+++ spec/features/player_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'the player hit points' do
   scenario 'when playing' do
-    visit('/')
-    fill_in('player_1', with: 'Bob')
-    fill_in('player_2', with: 'John')
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content("Bob: 60HP")
   end
 
--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,10 +1,6 @@
 feature 'Entering names' do
   scenario 'Submitting names' do
-    visit('/')
-    # within("#session")
-    fill_in('player_1', with: 'Bob')
-    fill_in('player_2', with: 'John')
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content ("Bob vs. John")
   end
 end
