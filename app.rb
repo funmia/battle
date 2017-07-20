506c59a6f7654867dc8138788c01e01e274a591e
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-07-20 15:34:41 +0100

    Skinny Controllers

--- app.rb
+++ app.rb
@@ -13,24 +13,22 @@ enable :sessions
   end
 
   post '/names' do
-    $player_1  = Player.new(params[:player_1])
-    $player_2  = Player.new(params[:player_2])
-    $game = Game.new
+    $game = Game.new(Player.new(params[:player_1]), Player.new(params[:player_2]))
     p params
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
+    @player_1 = $game.player_1.name
+    @player_2 = $game.player_2.name
     @commentator = ""
     erb(:play)
   end
 
   get '/attack' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
-    @player_2_points = $game.attack($player_2)
+    @player_1 = $game.player_1.name
+    @player_2 = $game.player_2.name
+    @player_2_points = $game.attack($game.player_2)
     @commentator = "#{@player_1} attacked #{@player_2}!!!"
     # erb(:attack)
     erb(:play)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,15 @@
 require 'game'
 
 describe 'Game' do
-  subject(:game) { Game.new }
-  let(:bob) {double(:Player, name: 'Bob', points: 60)}
+  subject(:game) { Game.new(bob, bill) }
+  let(:bob) {double(:player_1, name: 'Bob', points: 60)}
+  let(:bill) {double(:player_2, name: 'Bill', points: 60)}
+
+  describe '#initialize' do
+    it "accepts two Player instances" do
+      expect(game.player_1).to eq bob
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
